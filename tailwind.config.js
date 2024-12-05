4e50d4bea4a484c317d6bc980194c37a56029c37
Burhanuddin Dhar wala <burhandhar6004@gmail.com>
2024-12-05 18:39:46 +0530

    config file change

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import flowbite from 'flowbite/plugin';
 export default {
   content: [
     "./index.html",
@@ -9,7 +10,7 @@ export default {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin')
+    flowbite,
   ],
 }
 
