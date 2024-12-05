06bcd3fca8e19307aa484794d36007b085a57b8f
Burhanuddin Dhar wala <burhandhar6004@gmail.com>
2024-12-05 20:20:38 +0530

    design changes icon

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -7,6 +7,7 @@ import { fetchPosts } from "../Features/posts/postsSlice";
 import { fetchTodos } from "../Features/todos/todosSlice";
 import { fetchUsers } from "../Features/Users/userSlice";
 import { Link } from "react-router-dom";
+import { FaBookOpen, FaBuffer, FaCommentAlt, FaPhotoVideo, FaReact, FaUsers } from "react-icons/fa";
 
 const Dashboard = () => {
   const { users } = useSelector((state) => state.users);
@@ -32,8 +33,8 @@ const Dashboard = () => {
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
           <Link to={"/users"}>
             <div className="relative flex items-center justify-center h-24 w-72 rounded-xl bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-850 dark:to-gray-900 shadow-xl transform hover:scale-[1.02] transition-all duration-500 ease-in-out">
-              <p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 drop-shadow-sm">
-                Users {users.length}
+              <p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 flex items-center justify-center gap-5 drop-shadow-sm">
+              <FaUsers /> Users {users.length}
               </p>
               <div className="absolute inset-0 rounded-xl bg-gradient-to-br from-white/50 to-transparent dark:from-black/10 dark:to-transparent blur-lg"></div>
             </div>
@@ -41,8 +42,8 @@ const Dashboard = () => {
 
           <Link to={"/posts"}>
             <div className="relative flex items-center justify-center h-24 w-72 rounded-xl bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-850 dark:to-gray-900 shadow-xl transform hover:scale-[1.02] transition-all duration-500 ease-in-out">
-              <p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 drop-shadow-sm">
-                Posts {posts.length}
+              <p className="text-3xl font-semibold flex items-center justify-center gap-5 text-gray-800 dark:text-gray-100 drop-shadow-sm">
+              <FaBookOpen/>  Posts {posts.length}
               </p>
               <div className="absolute inset-0 rounded-xl bg-gradient-to-br from-white/50 to-transparent dark:from-black/10 dark:to-transparent blur-lg"></div>
             </div>
@@ -51,8 +52,8 @@ const Dashboard = () => {
           <Link to={"/comments"}>
             {" "}
             <div className="relative flex items-center justify-center h-24 w-72 rounded-xl bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-850 dark:to-gray-900 shadow-xl transform hover:scale-[1.02] transition-all duration-500 ease-in-out">
-              <p className="text-3xl font-semibold text-gray-800 dark:text-gray-100 drop-shadow-sm">
-                Comments {comments.length}
+              <p className="text-3xl flex items-center justify-center gap-5  font-semibold text-gray-800 dark:text-gray-100 drop-shadow-sm">
+               <FaCommentAlt/> Comments {comments.length}
               </p>
               <div className="absolute inset-0 rounded-xl bg-gradient-to-br from-white/50 to-transparent dark:from-black/10 dark:to-transparent blur-lg"></div>
             </div>
@@ -62,8 +63,8 @@ const Dashboard = () => {
         <Link to={"/photos"}>
           {" "}
           <div className="relative flex items-center justify-center h-48 mb-4 rounded-lg bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-900 dark:to-gray-900 shadow-2xl transform hover:scale-[1.05] transition-all duration-500 ease-in-out">
-            <p className="text-3xl font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
-              Photos {photos.length}
+            <p className="text-3xl flex items-center justify-center gap-5 font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
+              <FaPhotoVideo/>Photos {photos.length}
             </p>
             <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-white/20 to-black/10 dark:from-white/5 dark:to-black/20 blur-lg"></div>
           </div>
@@ -71,16 +72,16 @@ const Dashboard = () => {
         <div className="grid grid-cols-2 gap-4 mb-4">
          <Link to={"/todos"}>
          <div className="relative flex items-center justify-center rounded-lg bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-900 dark:to-gray-900 shadow-2xl transform hover:scale-[1.05] transition-all duration-500 ease-in-out h-28">
-            <p className="text-2xl font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
-              Todos {todos.length}
+            <p className="text-2xl flex items-center justify-center gap-5 font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
+            <FaReact/>  Todos {todos.length}
             </p>
             <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-white/20 to-black/10 dark:from-white/5 dark:to-black/20 blur-lg"></div>
           </div></Link>
 
           <Link to={"/albums"}>
           <div className="relative flex items-center justify-center rounded-lg bg-gradient-to-br from-white via-gray-100 to-gray-200 dark:from-gray-800 dark:via-gray-900 dark:to-gray-900 shadow-2xl transform hover:scale-[1.05] transition-all duration-500 ease-in-out h-28">
-            <p className="text-2xl font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
-              Albums {albums.length}
+            <p className="text-2xl flex items-center justify-center gap-5 font-semibold text-gray-800 dark:text-gray-200 drop-shadow-lg z-10">
+              <FaBuffer/> Albums {albums.length}
             </p>
             <div className="absolute inset-0 rounded-lg bg-gradient-to-br from-white/20 to-black/10 dark:from-white/5 dark:to-black/20 blur-lg"></div>
           </div></Link>
